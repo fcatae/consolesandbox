6678f0003531f00db0ae4566c0732a91f64f51cb
Fabricio Catae <fcatae@microsoft.com>
2017-11-29 21:06:35 -0200

    working test

--- PluginSandbox/Sandbox.cs
+++ PluginSandbox/Sandbox.cs
@@ -10,6 +10,18 @@ namespace PluginSandbox
 {
     public class Sandbox
     {
+        string _pluginDirectory;
+
+        public Sandbox()
+        {
+            _pluginDirectory = ".";
+        }
+
+        public Sandbox(string pluginDirectory)
+        {
+            _pluginDirectory = pluginDirectory;
+        }
+        
         public IPlugin CreateLocal<T>() where T: class
         {
             Type t = typeof(T);
@@ -30,7 +42,7 @@ namespace PluginSandbox
             string assemblyName = t.Assembly.FullName;
             string typeName = t.FullName;
 
-            var appDomain = CreateAppDomain(typeName, "plugin");
+            var appDomain = CreateAppDomain(typeName, _pluginDirectory);
             var remoteFactory = (PluginFactory<T>)appDomain.CreateInstanceAndUnwrap(assemblyName, typeName);
 
             return remoteFactory.Create();
@@ -41,7 +53,41 @@ namespace PluginSandbox
             var appInfo = new AppDomainSetup() { ApplicationBase = Path.GetFullPath(path) };
             var appDomain = AppDomain.CreateDomain(name, null, appInfo);
 
+            appDomain.AssemblyResolve += SandboxAssemblyResolver;
+
             return appDomain;
+        }
+
+        private static Assembly SandboxAssemblyResolver(object sender, ResolveEventArgs args)
+        {
+            AppDomain domain = (AppDomain)sender;
+            string baseDirectory = domain.BaseDirectory;
+
+            string dll = FindAssemblyFile(baseDirectory, args.Name);
+
+            var asm = Assembly.LoadFile(dll);
+
+            return asm;
+        }
+
+        private static string FindAssemblyFile(string baseDirectory, string assemblyName)
+        {
+            AssemblyName asmName = new AssemblyName(assemblyName);
+            string name = (new AssemblyName(assemblyName)).Name;
+            string defaultName = $"{name}.dll";
+
+            string[] validPaths = new string[] {
+                // Path.Combine(baseDirectory, "Plugin", this._plugin, $"{name}.dll"),
+                Path.Combine(baseDirectory, "Plugin", $"{name}.dll")
+            };
+
+            foreach (string path in validPaths)
+            {
+                if (File.Exists(path))
+                    return path;
+            }
+
+            return defaultName;
         }
 
         IPlugin CreateProxy(object obj)
